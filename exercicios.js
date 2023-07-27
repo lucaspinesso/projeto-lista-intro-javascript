91d1ccd93dba6f950317c01376bfe8bfc2973212
Lucas-Otavio-Pinesso-de-Morais <lucaspinessomorais@gmail.com>
2023-07-27 19:22:03 -0300

    exercício 9

--- exercicios.js
+++ exercicios.js
@@ -79,8 +79,7 @@ function checaStringsMesmoTamanho(string1, string2) {
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
-
+  return array[0]
 }
 
 // EXERCÍCIO 10
